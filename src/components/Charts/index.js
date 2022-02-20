85c0c688cfd9729848e474938fb59b4a42ca68ab
KONGARA JASWANTH <kongarajaswanth4@gmail.com>
2022-02-20 16:50:51 +0530

    Add files via upload

--- src/components/Charts/index.js
+++ src/components/Charts/index.js
@@ -1,1 +1,45 @@
+import  React from 'react';
+
+// Charts
+// eslint-disable-next-line no-unused-vars
+import  { Chart }  from 'chart.js/auto'
+import { Line } from 'react-chartjs-2';
+
+
+export const Charts = ({dailyData}) =>{
+    const lineChart = (
+        dailyData.length ?
+        (
+            <Line
+            data={{
+                labels: dailyData.map(({date})=> date),
+                datasets: [{
+                    data:dailyData.map(({confirmed})=> confirmed),
+                    label:'Infected',
+                    borderColor:'#3333ff',
+                    fill:true,
+                },
+                {
+                    data:dailyData.map(({recovered})=> recovered),
+                    label:'Recovered',
+                    borderColor:'green',
+                    fill:true,
+                },
+                {
+                    data:dailyData.map(({deceased})=> deceased),
+                    label:'Deaths',
+                    borderColor:'red',
+                    fill:true,
+                }
+            ]
+            }}
+            />
+        ):null
+    )
+    return(
+        <div>
+            {lineChart}
+        </div>
+    )
+}
 
